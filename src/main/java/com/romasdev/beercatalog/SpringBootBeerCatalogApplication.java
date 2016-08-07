5015493d303d450f7696f10fe9407cdf3b3ba70f
Romário Freitas <romarioaf1989@gmail.com>
2016-08-07 19:33:52 -0300

    Resolvendo problema no AutoConfiguration do spring boot.

--- src/main/java/com/romasdev/beercatalog/SpringBootBeerCatalogApplication.java
+++ src/main/java/com/romasdev/beercatalog/SpringBootBeerCatalogApplication.java
@@ -1,10 +1,12 @@
 package com.romasdev.beercatalog;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class SpringBootBeerCatalogApplication {
 
 	public static void main(String[] args) {
