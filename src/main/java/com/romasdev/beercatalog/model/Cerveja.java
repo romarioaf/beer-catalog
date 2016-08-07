b5ef07229b8d254c8805aa137540a0937c698518
Romário Freitas <romarioaf1989@gmail.com>
2016-08-07 19:34:51 -0300

    Mapeamento JPA da entidade cerveja e criação de enum para o tipo.

--- src/main/java/com/romasdev/beercatalog/model/Cerveja.java
+++ src/main/java/com/romasdev/beercatalog/model/Cerveja.java
@@ -1,5 +1,91 @@
 package com.romasdev.beercatalog.model;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.romasdev.beercatalog.model.enums.TipoCerveja;
+
+@Entity
+@Table(name = "cerveja")
 public class Cerveja {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Long id;
+	String titulo;
+	String descricao;
+	@Enumerated(EnumType.STRING)
+	TipoCerveja tipo;
+	String marca;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public TipoCerveja getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(TipoCerveja tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cerveja other = (Cerveja) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 
 }
---
+++ src/main/java/com/romasdev/beercatalog/model/enums/TipoCerveja.java
@@ -0,0 +1,6 @@
+package com.romasdev.beercatalog.model.enums;
+
+public enum TipoCerveja {
+	LAGER,
+	ALE;
+}
